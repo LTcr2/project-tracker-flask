1178177bc3859cb7d28df3faf7624aa99e7e0fff
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-30 12:55:49 -0700

    Added in base files

---
+++ hackbright_web.py
@@ -0,0 +1,52 @@
+"""A web application for tracking projects, students, and student grades."""
+
+from flask import Flask, request, render_template
+
+import hackbright
+
+app = Flask(__name__)
+
+
+@app.route("/student")
+def get_student():
+    """Show information about a student."""
+
+    github = request.args.get('github')
+
+    first, last, github = hackbright.get_student_by_github(github)
+
+    html = render_template('student_info.html',
+                            first=first,
+                            last=last,
+                            github=github)
+    return html
+
+    #return "{} is the GitHub account for {} {}".format(github, first, last)
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student"""
+
+    return render_template("student_search.html")
+
+
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    """Add a student."""
+
+    student_info = request.form('first_name', 'last_name', 'github')
+
+    first, last, github = hackbright.make_new_student(student_info)
+
+    html = render_template('student_info.html',
+                            first=first,
+                            last=last,
+                            github=github)
+    return html
+
+
+if __name__ == "__main__":
+    hackbright.connect_to_db(app)
+    app.run(debug=True)
+
+
